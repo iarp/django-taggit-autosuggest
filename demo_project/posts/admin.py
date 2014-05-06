7965998f9a7958330c8288eea767538da876cd86
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2014-05-06 12:37:16 +0200

    added notes to posts to test inlines

--- demo_project/posts/admin.py
+++ demo_project/posts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
+from models import Note
 from models import Post
 
 
-admin.site.register(Post)
+class NoteInline(admin.TabularInline):
+    model = Note
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
--- demo_project/posts/models.py
+++ demo_project/posts/models.py
@@ -4,7 +4,16 @@ from taggit_autosuggest.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=60)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     def __unicode__(self):
         return self.title
+
+
+class Note(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey('Post')
+    tags = TaggableManager(blank=True)
+
+    def __unicode__(self):
+        return self.text
