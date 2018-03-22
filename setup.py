26964b432a770bdcfddcc7ddac79ed6814ca4e8f
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2018-03-22 22:12:34 +0100

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = open('README.txt').read()
 
 setup(
     name='django-taggit-autosuggest',
-    version='0.3.2',
+    version='0.3.3',
     description='Autosuggestions for django-taggit',
     long_description=long_description,
     author='Fabian Topfstedt',
