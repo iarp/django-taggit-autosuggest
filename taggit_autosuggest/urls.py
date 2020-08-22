468bd538e73da2039f067867941c73a07147ac84
iarp <iarp.opensource@gmail.com>
2020-08-22 18:24:14 -0400

    fix(urls): non-tagmodel urls are not allowed.

--- taggit_autosuggest/urls.py
+++ taggit_autosuggest/urls.py
@@ -4,7 +4,6 @@ from .views import list_tags
 
 
 urlpatterns = [
-    url(r'^list/$', list_tags, name='taggit_autosuggest-list'),
     url(r'^list/(?P<tagmodel>[\._\w]+)/$', list_tags,
         name='taggit_autosuggest-list'),
 ]
